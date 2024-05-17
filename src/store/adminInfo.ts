09d1b305750181563fb5751426bc597c00474df7
ebokyung <dlqhrud303@naver.com>
2024-05-18 08:19:51 +0900

    feat: store 수정

--- src/store/adminInfo.ts
+++ src/store/adminInfo.ts
@@ -1,13 +1,18 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 interface AdminInfoState {
   nickname: string;
   setNickname: (nickname: string) => void;
 }
 
-const useAdminInfoStore = create<AdminInfoState>()((set) => ({
+const useAdminInfoStore = create<AdminInfoState>()(persist((set) => ({
   nickname: '',
   setNickname: (nickname) => set(() => ({ nickname })),
+})
+,{
+  name: 'admin-nickname',
+  storage: createJSONStorage(() => sessionStorage)
 }));
 
 export { useAdminInfoStore };
